da0f238bfcd13a894434a464e98c1223c214661d
William Osteimer Cardoso dos Anjos <wosteimer1@gmail.com>
2025-02-02 23:59:06 -0300

    refactor(Tiny Launcher): Removed unused imports

--- src/list_item.zig
+++ src/list_item.zig
@@ -1,9 +1,6 @@
 const std = @import("std");
 const c = @cImport({
     @cInclude("gtk/gtk.h");
-    @cInclude("gtk4-layer-shell/gtk4-layer-shell.h");
-    @cInclude("gio/gdesktopappinfo.h");
-    @cInclude("gobject/gobject.h");
 });
 
 pub const TsListItem = struct {

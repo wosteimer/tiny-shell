f82840f595714f304883ee441d3fcad8eeb94850
William Osteimer Cardoso dos Anjos <wosteimer1@gmail.com>
2025-02-06 15:29:37 -0300

    refactor: refactor in build.zig file

--- build.zig
+++ build.zig
@@ -3,24 +3,12 @@ const std = @import("std");
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
-
-    // const lib = b.addSharedLibrary(.{
-    //     .name = "tiny-shell",
-    //     .root_source_file = b.path("src/root.zig"),
-    //     .target = target,
-    //     .optimize = optimize,
-    // });
-    // lib.linkLibC();
-    // lib.linkSystemLibrary("gmodule-export-2.0");
-    // b.installArtifact(lib);
-
     const gen_resources_header = b.addSystemCommand(&.{
         "glib-compile-resources",
         "--generate-header",
         "data.gresource.xml",
     });
     gen_resources_header.setCwd(b.path("data"));
-
     const gen_resources_source = b.addSystemCommand(&.{
         "glib-compile-resources",
         "--generate-source",
@@ -28,63 +16,37 @@ pub fn build(b: *std.Build) void {
     });
     gen_resources_source.setCwd(b.path("data"));
     gen_resources_source.step.dependOn(&gen_resources_header.step);
-
-    const tiny_launcher = b.addExecutable(.{
-        .name = "tiny-launcher",
-        .root_source_file = b.path("src/tiny_launcher.zig"),
+    const ts_launcher = b.addExecutable(.{
+        .name = "ts-launcher",
+        .root_source_file = b.path("src/ts-launcher.zig"),
         .target = target,
         .optimize = optimize,
     });
-    tiny_launcher.addCSourceFile(.{ .file = b.path("data/data.c") });
-    tiny_launcher.addIncludePath(b.path("data"));
-    // tiny_launcher.linkLibrary(lib);
-    tiny_launcher.linkLibC();
-    tiny_launcher.linkSystemLibrary("libadwaita-1");
-    tiny_launcher.linkSystemLibrary("gtk4");
-    tiny_launcher.linkSystemLibrary("gtk4-layer-shell");
-    tiny_launcher.linkSystemLibrary("gio-unix-2.0");
-    tiny_launcher.linkSystemLibrary("gobject-2.0");
-    b.installArtifact(tiny_launcher);
-
-    const tiny_launcher_cmd = b.addRunArtifact(tiny_launcher);
-
-    tiny_launcher_cmd.step.dependOn(&gen_resources_source.step);
-
+    ts_launcher.addCSourceFile(.{ .file = b.path("data/data.c") });
+    ts_launcher.addIncludePath(b.path("data"));
+    ts_launcher.linkLibC();
+    ts_launcher.linkSystemLibrary("libadwaita-1");
+    ts_launcher.linkSystemLibrary("gtk4");
+    ts_launcher.linkSystemLibrary("gtk4-layer-shell");
+    ts_launcher.linkSystemLibrary("gio-unix-2.0");
+    ts_launcher.linkSystemLibrary("gobject-2.0");
+    b.installArtifact(ts_launcher);
+    const ts_launcher_cmd = b.addRunArtifact(ts_launcher);
+    ts_launcher_cmd.step.dependOn(&gen_resources_source.step);
     if (b.args) |args| {
-        tiny_launcher_cmd.addArgs(args);
+        ts_launcher_cmd.addArgs(args);
     }
-
-    const run_step = b.step("run-launcher", "Run the app");
-    run_step.dependOn(&tiny_launcher_cmd.step);
-
-    // const lib_unit_tests = b.addTest(.{
-    //     .root_source_file = b.path("src/root.zig"),
-    //     .target = target,
-    //     .optimize = optimize,
-    // });
-    //
-    // const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
-    //
-    // const exe_unit_tests = b.addTest(.{
-    //     .root_source_file = b.path("src/main.zig"),
-    //     .target = target,
-    //     .optimize = optimize,
-    // });
-    //
-    // const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
-    //
-    // const test_step = b.step("test", "Run unit tests");
-    // test_step.dependOn(&run_lib_unit_tests.step);
-    // test_step.dependOn(&run_exe_unit_tests.step);
-    const tsmodel_unit_tests = b.addTest(.{
+    const run_step = b.step("run", "Run the app");
+    run_step.dependOn(&ts_launcher_cmd.step);
+    const ts_model_unit_tests = b.addTest(.{
         .root_source_file = b.path("src/ts-model.zig"),
         .target = target,
         .optimize = optimize,
     });
-    tsmodel_unit_tests.linkLibC();
-    tsmodel_unit_tests.linkSystemLibrary("gobject-2.0");
-    tsmodel_unit_tests.linkSystemLibrary("gio-unix-2.0");
-    const run_tsmodel_unit_tests = b.addRunArtifact(tsmodel_unit_tests);
+    ts_model_unit_tests.linkLibC();
+    ts_model_unit_tests.linkSystemLibrary("gobject-2.0");
+    ts_model_unit_tests.linkSystemLibrary("gio-unix-2.0");
+    const run_ts_model_unit_tests = b.addRunArtifact(ts_model_unit_tests);
     const test_step = b.step("test", "Run unit tests");
-    test_step.dependOn(&run_tsmodel_unit_tests.step);
+    test_step.dependOn(&run_ts_model_unit_tests.step);
 }

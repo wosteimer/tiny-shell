32525a7dc3a1918139903da7c7ec2f7c2d5f75ae
William Osteimer Cardoso dos Anjos <wosteimer1@gmail.com>
2025-02-10 07:39:54 -0300

    feat: Now the app realizes if there is already an instance and if so, it just displays the window of the already instantiated app

--- src/ts-launcher.zig
+++ src/ts-launcher.zig
@@ -1,12 +1,22 @@
 const std = @import("std");
 const c = @import("c.zig");
+const g = @import("g-utils.zig");
 const TsLauncherWindow = @import("ts-launcher-window.zig").TsLauncherWindow;
 const TS_LAUNCHER_WINDOW = @import("ts-launcher-window.zig").TS_LAUNCHER_WINDOW;
 
 const APP_ID = "com.github.wosteimer.tiny.launcher";
 const DBUS_OBJECT_PATH = "/com/github/wosteimer/tiny/launcher";
 
+var interface_info: ?*c.GDBusNodeInfo = null;
+const interface_vtable = c.GDBusInterfaceVTable{
+    .method_call = @ptrCast(&onMethodCall),
+};
+
 pub fn main() !void {
+    if (isRunning()) {
+        showWindow();
+        return;
+    }
     const app = c.adw_application_new(
         APP_ID,
         c.G_APPLICATION_DEFAULT_FLAGS,
@@ -22,6 +32,8 @@ pub fn main() !void {
     );
     _ = c.g_application_run(@ptrCast(app), 0, null);
 }
+
+// TODO: Error handling for all funcions below
 
 fn activate(app: *c.GtkApplication, _: c.gpointer) callconv(.C) void {
     const css_provider = c.gtk_css_provider_new();
@@ -43,7 +55,7 @@ fn activate(app: *c.GtkApplication, _: c.gpointer) callconv(.C) void {
         window,
         c.GTK_LAYER_SHELL_KEYBOARD_MODE_EXCLUSIVE,
     );
-    //c.gtk_window_present(window);
+    c.gtk_window_present(window);
     _ = c.g_bus_own_name(
         c.G_BUS_TYPE_SESSION,
         APP_ID,
@@ -61,29 +73,89 @@ fn onBusAcquired(
     _: [*:0]const u8,
     user_data: c.gpointer,
 ) callconv(.C) void {
-    _ = c.g_dbus_connection_signal_subscribe(
+    const xml = (
+        \\<node>
+        \\  <interface name="{s}">
+        \\      <method name="ShowWindow"/>
+        \\  </interface>
+        \\</node>
+    );
+    var buf = std.mem.zeroes([255]u8);
+    _ = std.fmt.bufPrint(&buf, xml, .{APP_ID}) catch {
+        @panic("error trying to generate dbus node info");
+    };
+    if (interface_info == null) {
+        interface_info = c.g_dbus_node_info_new_for_xml(&buf, null);
+    }
+    _ = c.g_dbus_connection_register_object(
         connection,
-        null,
-        APP_ID,
-        "Show",
         DBUS_OBJECT_PATH,
-        null,
-        c.G_DBUS_SIGNAL_FLAGS_NONE,
-        @ptrCast(&onShowSignal),
+        interface_info.?.*.interfaces[0],
+        &interface_vtable,
         user_data,
+        null,
         null,
     );
 }
 
-fn onShowSignal(
+fn onMethodCall(
     _: *c.GDBusConnection,
     _: [*:0]const u8,
     _: [*:0]const u8,
     _: [*:0]const u8,
-    _: [*:0]const u8,
+    method_name: [*:0]const u8,
     _: *c.GVariant,
+    invocation: ?*c.GDBusMethodInvocation,
     user_data: c.gpointer,
 ) callconv(.C) void {
-    const window = TS_LAUNCHER_WINDOW(user_data);
-    window.show();
+    if (std.mem.eql(u8, std.mem.span(method_name), "ShowWindow")) {
+        const window = TS_LAUNCHER_WINDOW(user_data);
+        window.show();
+    }
+    c.g_dbus_method_invocation_return_value(invocation, null);
+}
+
+fn isRunning() bool {
+    const bus = c.g_bus_get_sync(c.G_BUS_TYPE_SESSION, null, null);
+    if (bus == null) return false;
+    defer c.g_object_unref(g.G_OBJECT(bus));
+    const result = c.g_dbus_connection_call_sync(
+        bus,
+        "org.freedesktop.DBus",
+        "/org/freedesktop/DBus",
+        "org.freedesktop.DBus",
+        "NameHasOwner",
+        c.g_variant_new("(s)", APP_ID),
+        c.G_VARIANT_TYPE("(b)"),
+        c.G_DBUS_CALL_FLAGS_NONE,
+        -1,
+        null,
+        null,
+    );
+    if (result == null) return false;
+    var is_running: i32 = 0;
+    c.g_variant_get(result, "(b)", &is_running);
+    defer c.g_variant_unref(result);
+    return is_running != 0;
+}
+
+fn showWindow() void {
+    const bus = c.g_bus_get_sync(c.G_BUS_TYPE_SESSION, null, null);
+    if (bus == null) return;
+    defer c.g_object_unref(g.G_OBJECT(bus));
+    const result = c.g_dbus_connection_call_sync(
+        bus,
+        APP_ID,
+        DBUS_OBJECT_PATH,
+        APP_ID,
+        "ShowWindow",
+        null,
+        null,
+        c.G_DBUS_CALL_FLAGS_NONE,
+        -1,
+        null,
+        null,
+    );
+    if (result == null) return;
+    defer c.g_variant_unref(result);
 }
